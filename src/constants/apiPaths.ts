68d9c73eb67e822c161b60cdcaf27b90c7f9c5b5
anahit <anahit.meghri@gmail.com>
2026-05-03 15:54:19 +0200

    Changed BE path.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   products:
-    "https://wkl6dv2dzd.execute-api.eu-west-1.amazonaws.com/dev/products",
+    "https://ud5dinz9m3.execute-api.eu-west-1.amazonaws.com/dev/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://wkl6dv2dzd.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://ud5dinz9m3.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
